0ef2437e9cdf9cb6fdb6ed782ea2b1fbf6bd4d4c
Nick Bruun <nick@bruun.co>
2018-07-03 10:58:33 -0700

    Support Python 3.7+.

--- idna_ssl.py
+++ idna_ssl.py
@@ -1,31 +1,40 @@
 import ssl
+import sys
 
 import idna
 
 __version__ = '1.0.1'
 
-real_match_hostname = ssl.match_hostname
 
+if sys.version_info[0] == 3 and sys.version_info[1] < 7:
+    real_match_hostname = ssl.match_hostname
 
-def patched_match_hostname(cert, hostname):
-    try:
-        hostname = idna.encode(hostname, uts46=True).decode('ascii')
-    except UnicodeError:
-        hostname = hostname.encode('idna').decode('ascii')
+    def patched_match_hostname(cert, hostname):
+        try:
+            hostname = idna.encode(hostname, uts46=True).decode('ascii')
+        except UnicodeError:
+            hostname = hostname.encode('idna').decode('ascii')
 
-    return real_match_hostname(cert, hostname)
+        return real_match_hostname(cert, hostname)
 
+    def patch_match_hostname():
+        if hasattr(ssl.match_hostname, 'patched'):
+            return
 
-def patch_match_hostname():
-    if hasattr(ssl.match_hostname, 'patched'):
-        return
+        ssl.match_hostname = patched_match_hostname
+        ssl.match_hostname.patched = True
 
-    ssl.match_hostname = patched_match_hostname
-    ssl.match_hostname.patched = True
+    def reset_match_hostname():
+        if not hasattr(ssl.match_hostname, 'patched'):
+            return
 
+        ssl.match_hostname = real_match_hostname
+else:
+    def patched_match_hostname(cert, hostname):
+        return ssl.match_hostname(cert, hostname)
 
-def reset_match_hostname():
-    if not hasattr(ssl.match_hostname, 'patched'):
-        return
+    def patch_match_hostname():
+        pass
 
-    ssl.match_hostname = real_match_hostname
+    def reset_match_hostname():
+        pass
